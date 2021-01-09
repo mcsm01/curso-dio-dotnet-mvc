c5d160d15dd27bbd22c730566e824cd3fc26b2ca
mcsm01 <mayramedeiros83@gmail.com>
2021-01-08 22:51:31 -0300

    primeiro commit

---
+++ CursoMVC/CursoMVC/Models/Context.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Context: DbContext
+    {
+        public DbSet<Categoria> Categorias { get;  set; }
+        public DbSet<Produto> Produtos { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {}
+
+        public virtual void SetModified(object entity)
+        {
+            Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ CursoTest/CancellationTolken.cs
@@ -0,0 +1,6 @@
+﻿namespace CursoTest
+{
+    internal class CancellationTolken
+    {
+    }
+}
